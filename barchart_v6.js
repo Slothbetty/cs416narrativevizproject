d4938a68a0bee514df67c1fc5abeabc52b45cca6
Bei Zhao <beizhao3@illinois.edu>
2022-07-26 23:46:10 -0700

    Downgrade d3 v6 to d3 v4 and add tooltip successfully.

--- barchart_v6.js
+++ barchart_v6.js
@@ -12,7 +12,7 @@ const svg = d3.select("#my_dataviz")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Parse the Data
-d3.csv("ds_salaries.csv", function(data) {
+d3.csv("ds_salaries.csv").then(function (data) {
     console.log(data);
     console.log(typeof data);
 
@@ -31,17 +31,13 @@ d3.csv("ds_salaries.csv", function(data) {
                 }
             }
         }
-        const arr = Array.from(year_count_map, ([key, value]) => {
-            return {['year']: key, ['count']:value };
-          });
-        console.log(arr);
 
         // Add X axis
         const x = d3.scaleLinear()
             .domain([0, 600])
             .range([0, width]);
         svg.append("g")
-            .attr("transform", "translate(0," + height + ")")
+            .attr("transform", `translate(0, ${height})`)
             .call(d3.axisBottom(x))
             .selectAll("text")
             .attr("transform", "translate(-10,0)rotate(-45)")
@@ -52,13 +48,16 @@ d3.csv("ds_salaries.csv", function(data) {
             .range([0, height])
             .domain([...year_count_map.keys()])
             .padding(.1);
+
+        console.log(year_count_map);
+
         svg.append("g")
             .call(d3.axisLeft(y))
 
-        var tooltip = d3.select("body").append("div").attr("class", "toolTip");
+
         // variable u: map data to existing bars
         var u = svg.selectAll("rect")
-            .data(arr)
+            .data(year_count_map)
 
         // update bars
         u
@@ -66,18 +65,10 @@ d3.csv("ds_salaries.csv", function(data) {
             .append("rect")
             .merge(u)
             .attr("x", function (d) { return x(0); })
-            .attr("y", function (d) { return y(d.year); })
-            .attr("width", function (d) { return x(d.count); })
+            .attr("y", function (d) { return y(d[0]); })
+            .attr("width", function (d) { return x(d[1]); })
             .attr("height", function (d) { return y.bandwidth(); })
             .attr("fill", "#69b3a2")
-            .on("mousemove", function(d){
-                tooltip
-                  .style("left", d3.event.pageX - 50 + "px")
-                  .style("top", d3.event.pageY - 70 + "px")
-                  .style("display", "inline-block")
-                  .html((d.area) + "<br>" + "£" + (d.value));
-            })
-            .on("mouseout", function(d){ tooltip.style("display", "none");});
             
 
         var slider = d3.select('#salary');
@@ -86,6 +77,37 @@ d3.csv("ds_salaries.csv", function(data) {
             draw(this.value);
         });
 
+        // // ----------------
+        // // Create a tooltip
+        // // ----------------
+        // var tooltip = d3.select("#my_dataviz")
+        // .append("div")
+        // .style("opacity", 0)
+        // .attr("class", "tooltip")
+        // .style("background-color", "white")
+        // .style("border", "solid")
+        // .style("border-width", "1px")
+        // .style("border-radius", "5px")
+        // .style("padding", "10px")
+
+        // // Three function that change the tooltip when user hover / move / leave a cell
+        // var mouseover = function(d) {
+        //     console.log("hit mouseover");
+        // var subgroupName = d3.select(this.parentNode).datum().key;
+        // var subgroupValue = d.data[subgroupName];
+        // tooltip
+        //     .html("subgroup: " + subgroupName + "<br>" + "Value: " + subgroupValue)
+        //     .style("opacity", 1)
+        // }
+        // var mousemove = function(d) {
+        // tooltip
+        //     .style("left", (d3.mouse(this)[0]+90) + "px") // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
+        //     .style("top", (d3.mouse(this)[1]) + "px")
+        // }
+        // var mouseleave = function(d) {
+        // tooltip
+        //     .style("opacity", 0)
+        // }
     }
 
 
